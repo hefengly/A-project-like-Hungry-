d7e944ee7e852920bc0ad6f590176e34e257340f
hefengly <1047568365@qq.com>
2017-10-02 21:51:18 +0800

    finish goodsmenu
    finish goodsmenu but not add the shopping

---
+++ sell/src/components/typeIcon/typeIcon.vue
@@ -0,0 +1,94 @@
+<template>
+  <span v-show="iconType>=0" class="icon" :class="classMap[iconType]+'-'+iconNumber"></span>
+</template>
+
+<script type="text/acmascript-6">
+  export default {
+    props: {
+      iconType: {
+				type: Number
+			},			
+			iconSize: {
+				type: Number
+			},
+			iconNumber: {
+				type: Number
+			}
+		},
+		created() {
+			this.classMap = ['decrease',"discount",'special','guarantee','invoice']
+		}
+	};
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+	@import "../../common/stylus/mixin.styl"
+
+
+	.icon
+		display: inline-block
+		vertical-align:top
+		margin-right:4px
+		background-size: 12px 12px
+		background-repeat: no-repeat
+		&.decrease-1
+			width:12px
+			height:12px
+			bg-image("decrease_1")
+		&.discount-1
+			width:12px
+			height:12px		
+			bg-image('discount_1')
+		&.guarantee-1
+			width:12px
+			height:12px		
+			bg-image('guarantee_1')
+		&.invoice-1
+			width:12px
+			height:12px		
+			bg-image('invoice_1')
+		&.special-1
+			width:12px
+			height:12px		
+			bg-image('special_1')
+		&.decrease-2
+			width:18px
+			height:18px
+			bg-image('decrease_2')
+		&.discount-2
+			width:18px
+			height:18px
+			bg-image('discount_2')
+		&.guarantee-2
+			width:18px
+			height:18px
+			bg-image('guarantee_2')
+		&.invoice-2
+			width:18px
+			height:18px
+			bg-image('invoice_2')
+		&.special-2
+			width:18px
+			height:18px
+			bg-image('special_2')
+		&.decrease-3
+			width:12px
+			height:12px
+			bg-image('decrease_3')
+		&.discount-3
+			width:12px
+			height:12px		
+			bg-image('discount_3')
+		&.guarantee-3
+			width:12px
+			height:12px		
+			bg-image('guarantee_3')
+		&.invoice-3
+			width:12px
+			height:12px		
+			bg-image('invoice_3')
+		&.special-3
+			width:12px
+			height:12px		
+			bg-image('special_3')			
+</style>
